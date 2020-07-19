5aae980942d65805b91b4f4edab51039e656a8d4
Francesco <med.dama@gmail.com>
2020-07-19 17:31:23 +0100

    Create a new property date with getters and setters in Booking class

--- Booking/Booking.cs
+++ Booking/Booking.cs
@@ -2,6 +2,17 @@ namespace Booking
 {
     public class Booking
     {
-        
+        private string _date;
+
+        public Booking(string date)
+        {
+            _date = date;
+        }
+
+        public string Date
+        {
+            get => _date;
+            set => _date = value;
+        }
     }
 }
