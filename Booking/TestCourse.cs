1fd1cfa0848bcaebc8f2c0802e9cc3b209d48cab
Francesco <med.dama@gmail.com>
2020-07-19 17:15:10 +0100

    Test canHaveName passed

--- Booking/TestCourse.cs
+++ Booking/TestCourse.cs
@@ -1,8 +1,24 @@
+using System.Security.Cryptography;
+using Xunit;
+
 namespace Booking
 {
     public class TestCourse
     {
         private readonly Course _sut;
-        
+
+        public TestCourse()
+        {
+            _sut = new Course("Java", "Glasgow", 4);
+            
+        }
+
+        [Fact]
+        public void canHaveName()
+        {
+            Assert.Equal("Java", _sut.Name);
+        }
     }
+    
+    
 }
