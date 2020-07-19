dab1e767f321c03a5ee182b805e2ef5b4e0d5cdb
Francesco <med.dama@gmail.com>
2020-07-19 17:33:54 +0100

    Create filled Customer class wiht property ,getters and setters

--- Booking/Customer.cs
+++ Booking/Customer.cs
@@ -2,6 +2,33 @@ namespace Booking
 {
     public class Customer
     {
-        
+        private string _name;
+        private string _town;
+        private int _age;
+
+        public Customer(string name, string town, int age)
+        {
+            _name = name;
+            _town = town;
+            _age = age;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Town
+        {
+            get => _town;
+            set => _town = value;
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
     }
 }
