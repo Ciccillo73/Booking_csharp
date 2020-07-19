279b0b6bd65563c353cdac1f8ab306b914ee0fd4
Francesco <med.dama@gmail.com>
2020-07-19 17:36:49 +0100

    Create List in Booking class

--- Booking/Booking.cs
+++ Booking/Booking.cs
@@ -1,12 +1,16 @@
+using System.Collections.Generic;
+
 namespace Booking
 {
     public class Booking
     {
         private string _date;
-
+        private List<Customer> _customers;
+        
         public Booking(string date)
         {
             _date = date;
+            _customers = new List<Customer>();
         }
 
         public string Date
@@ -14,5 +18,7 @@ namespace Booking
             get => _date;
             set => _date = value;
         }
+        
+        
     }
 }
